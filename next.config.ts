111804a61d711be287402e9f6f1b00ec92f8956e
tkddnjs365 <tkddnjs365@naver.com>
2025-07-22 11:04:40 +0900

    수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: '/portfolio', // GitHub Pages 서브패스용
+  assetPrefix: '/portfolio', // 정적 파일 경로 설정
 };
 
 export default nextConfig;
